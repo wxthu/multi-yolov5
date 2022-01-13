28e75ff218dc8e318c193161caa8dd80c02bf93d
wxthu <tsinghuawangx@163.com>
2022-01-13 21:42:52 +0800

    fix some bugs and modify Detect.run interface

--- detect.py
+++ detect.py
@@ -68,24 +68,16 @@ class Detect:
         return img
 
     @torch.no_grad()
-    def run(self,
-            weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
-            imgsz=(640, 640),  # inference size (height, width)
-            device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-            augment=False,  # augmented inference
-            visualize=False,  # visualize features
-            half=False,  # use FP16 half-precision inference
-            dnn=False,  # use OpenCV DNN for ONNX inference
-            ):
-
+    def run(self, **kwargs):
+    
         # Load model
-        self.device = select_device(device)
-        self.model = DetectMultiBackend(weights, device=self.device, dnn=dnn)
-        stride, names, pt, jit, onnx, engine = self.model.stride, self.model.names, self.model.pt, self.model.jit, self.model.onnx, self.model.engine
-        self.imgsz = check_img_size(imgsz, s=stride)  # check image size
+        self.device = select_device(kwargs['device'])
+        self.model = DetectMultiBackend(kwargs['weights'], device=self.device, dnn=kwargs['dnn'])
+        stride, pt, jit, onnx, engine = self.model.stride, self.model.pt, self.model.jit, self.model.onnx, self.model.engine
+        self.imgsz = check_img_size(kwargs['imgsz'], s=stride)  # check image size
 
         # Half
-        half &= (pt or jit or onnx or engine) and self.device.type != 'cpu'  # FP16 supported on limited backends with CUDA
+        half = kwargs['half'] & (pt or jit or onnx or engine) and self.device.type != 'cpu'  # FP16 supported on limited backends with CUDA
         if pt or jit:
             self.model.model.half() if half else self.model.model.float()
 
@@ -93,9 +85,9 @@ class Detect:
 
         # Run inference
         dt = []
-        self.model.warmup(imgsz=(1, 3, *imgsz), half=half)  # warmup
+        self.model.warmup(imgsz=(1, 3, *self.imgsz), half=half)  # warmup
         t1 = time_sync()
-        im = torch.from_numpy(self.img).to(device)
+        im = torch.from_numpy(self.img).to(self.device)
         im = im.half() if half else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
@@ -105,7 +97,7 @@ class Detect:
         dt.append(t2 - t1)
 
         # Inference
-        pred = self.model(im, augment=augment, visualize=visualize)
+        self.model(im, augment=kwargs['augment'], visualize=kwargs['visualize'])
         t3 = time_sync()
         dt.append(t3 - t2)
 
@@ -117,10 +109,29 @@ class Detect:
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')   
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
     opt = parser.parse_args()
