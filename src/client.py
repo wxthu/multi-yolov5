5599bb95df699728937431affb0a7c6194252622
azuna <32482525+monster119120@users.noreply.github.com>
2022-01-14 13:20:18 +0800

    更新src/client.py
    把client.py封装成类，只需修改#TODO部分内容

--- src/client.py
+++ src/client.py
@@ -1,43 +1,62 @@
 import socket
 from _thread import *
 import time
-
-client_num = 8
-
-
-# 创造一个新的client
-def make_client(client_id):
-    socketObject = socket.socket()
-    socketObject.connect(("localhost", 35491))
-    print("Connected to localhost")
-
-    # 发送握手头信息
-    HTTPMessage = "GET / HTTP/1.1\r\nHost: localhost\r\n Connection: close\r\n\r\n"
-    socketObject.sendall(str.encode(HTTPMessage))
-
-    # Receive the data
-    while True:
-        # TODO 设置间隔频率
-        time.sleep(2)
-
-        # TODO 要发给server的数据data
-        socketObject.send(str.encode(client_id))
-
-        # TODO 从server收到的结果
-        res = socketObject.recv(1024)
-
-        print(res)
-        if res == b'':
-            print("Connection closed")
-            break
-    socketObject.close()
+import numpy as np
+import pickle
+import os
 
 
-for i in range(client_num):
-    # 启动一个新的client
-    start_new_thread(make_client, (str(i),))
+class Clients:
+    def __init__(self, client_num=8, host='localhost', port=35491, freq=1.0, buff_size=4096):
+        self.host = host
+        self.client_num = client_num
+        self.port = port
+        self.freq = freq   # 每个线程每隔freq秒发送一次图片
+        self.buff_size = buff_size
 
+    def run(self):
+        for i in range(self.client_num):
+            # 启动一个新的client
+            start_new_thread(self.single_client_thread, (i,))
+
+    def send_np_array(self, handler, data):
+        serialized_data = pickle.dumps(data, protocol=2)
+        handler.sendall(serialized_data)
+
+    def recv_np_array(self, handler):
+        res = b''
+        res += handler.recv(self.buff_size)
+        res_data = pickle.loads(res, encoding='bytes')
+        return res_data
+
+    def single_client_thread(self, client_id:int):
+        # 连接至服务器
+        socket_handler = socket.socket()
+        socket_handler.connect((self.host, self.port))
+        print("Connected to localhost")
+
+        # 不断地在loop内发送数据到服务器
+        while True:
+            time.sleep(self.freq)
+
+            # TODO 要发给server的数据data
+            data = np.ones((client_id + 1,))*client_id
+            self.send_np_array(socket_handler, data)
+
+            # TODO 从server收到的结果
+            res_data = self.recv_np_array(socket_handler)
+
+            print('client', client_id, '收到的数据', res_data, '\n')
+            if type(res_data) is not np.ndarray:
+                print("Connection closed")
+                break
+        socket_handler.close()
+
+
+C = Clients()
+C.run()
+
+# 等待所有线程退出
 while True:
-    stop_flag = input()
-    if stop_flag == '':
+    if input() == '':
         break
