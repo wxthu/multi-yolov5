b07419c98218213f64f74dd73d5536968687459d
airT4 <tsinghuawangx@163.com>
2022-04-26 20:25:10 +0800

    add batched request config

--- controller.py
+++ controller.py
@@ -6,13 +6,14 @@ class Controller:
     """
     作为Controller, 监视所有的Worker
     """
-    def __init__(self, detector_num=3, img_num=250, c2wQueues=None, w2cQueues=None, imgQueues=None, 
-                weights=None, prices=None, mem=1024):
+    def __init__(self, detector_num=3, img_num=800, mem=1024, config=None, c2wQueues=None,
+                w2cQueues=None, imgQueues=None):
         """ 
         two-way communiaction to prevent message contention
         """
-        self.weights = weights
-        self.prices = prices
+        self.weights = []
+        self.prices = []
+        self.batches = []
         self.memory = mem  # memory contraint
         self.detector_num = detector_num
         self.act_ids = [] 
@@ -26,6 +27,7 @@ class Controller:
         self.cursor = 0 # add cursor for cmds
         self.task_num = 0 # record the number of tasks at the moment
         self.total_requests = 0
+        self.config = config  # list, each element is dictionary
         
     def initialization(self):
         for q in range(len(self.imgQueues)):
@@ -34,7 +36,7 @@ class Controller:
                 # q.put(np.zeros(shape=(1920, 1080, 3)))
                 self.imgQueues[q].put(i+1)
 
-        df = pd.read_csv('mock_request_rate.csv')
+        df = pd.read_csv('mock_batched_request_rate.csv')
         for col in df:
             self.cmds.append(df[col].values)
 
@@ -59,9 +61,23 @@ class Controller:
             cmd = self.cmds[self.cursor]
 
             # add 0 in the front of the list for dynamic programming
-            wts = [0] + [self.weights[i] for i, elem in enumerate(cmd) if elem]
-            prc = [0] + [self.prices[i] for i, elem in enumerate(cmd) if elem]
-            candidate = [i for i, elem in enumerate(cmd) if elem]
+            wts = [0] 
+            prc = [0]
+            candidate = []
+            for i, batch in enumerate(cmd):
+                if batch:
+                    candidate.append(i)
+                    self.batches.append(batch)
+                    self.weights.append(self.config[i][batch][0])
+                    self.prices.append(self.config[i][batch][1]['infer_time'])
+                    wts.append(self.config[i][batch][0])
+                    prc.append(self.config[i][batch][1]['infer_time'])
+                else:
+                    # when detector i is not selected , we still add one element in the list 
+                    # to ensure the consistency of indexing
+                    self.weights.append(self.config[i][1][0])
+                    self.prices.append(self.config[i][1][1]['infer_time'])
+            
             candidate.sort(key=self.compare_p)
             
             self.task_num = len(candidate)
@@ -79,6 +95,10 @@ class Controller:
             for i in range(len(self.act_ids)):
                 self.c2wQueues[self.act_ids[i]].put('to_load')
                 self.c2wQueues[self.act_ids[i]].put('to_infer')
+
+                # here we treat list as queue and pop the head element
+                assert len(self.batches) > 0
+                self.c2wQueues[self.act_ids[i]].put(self.batches.pop(0))
         
         popLists = []
         for j in range(len(self.act_ids)):
@@ -91,16 +111,16 @@ class Controller:
 
         self.popWorkers(popLists)
         reMem = self.remainingMemory()
-        # print(f"**** remkk;k;k; {reMem}")
         while reMem > 0:
             if len(self.wait_ids) > 0:
-                # print(f'wait id : {self.wait_ids}')
                 for ind in self.wait_ids:
-                    # print(f'current wait : {ind}')
                     if self.weights[ind] <= reMem:
                         new_id = self.wait_ids.pop(0)   # get new workers from head of queue
                         self.c2wQueues[new_id].put('to_load')
                         self.c2wQueues[new_id].put('to_infer')
+
+                        assert len(self.batches) > 0
+                        self.c2wQueues[self.act_ids[i]].put(self.batches.pop(0))
                         self.act_ids.append(new_id)
                         print(f'add new worker {new_id} success !!!')
                         reMem -= self.weights[new_id]
--- scheduling.py
+++ scheduling.py
@@ -143,18 +143,16 @@ def runController(workers_num,
                   c2wQueues,
                   w2cQueues, 
                   imgQueues,
-                  weights, 
-                  prices,
-                  total_memory):
-    controller = Controller(detector_num=workers_num, img_num=image_num*video_num, c2wQueues=c2wQueues,
-                            w2cQueues=w2cQueues, imgQueues=imgQueues, weights=weights, prices=prices,
-                            mem=total_memory)
+                  total_memory,
+                  config):
+    controller = Controller(detector_num=workers_num, img_num=image_num*video_num, mem=total_memory, config=config,
+                            c2wQueues=c2wQueues, w2cQueues=w2cQueues, imgQueues=imgQueues)
     controller.run()
 
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--bs', type=int, default=1, help='batch size of img')
-    parser.add_argument('--img_num', type=int, default=50, help='the number of img sent by each client')
+    parser.add_argument('--img_num', type=int, default=200, help='the number of img sent by each client')
     parser.add_argument('--videos', type=int, default=4, help='the number of video stream')
     parser.add_argument('--workers', type=int, default=6, help='the number of detector')
     parser.add_argument('--memory', type=int, default=1500, help='available gpu memory in the server')
@@ -174,6 +172,7 @@ def main(opt):
     video_num = args_dict['videos']
     total_memory = args_dict['memory']
     
+    config = [x[2] for x in models]
     # record all workers timestamp
     time_stamp = Manager().list()
     MAX_Q_NUM = 1000
@@ -188,9 +187,8 @@ def main(opt):
                                                      c2wQueues,
                                                      w2cQueues,
                                                      imgQueues,
-                                                     weights,
-                                                     prices,
-                                                     total_memory
+                                                     total_memory,
+                                                     config
                                                      )
                          )
     
@@ -229,22 +227,58 @@ if __name__ == "__main__":
     yolov5s = DetectMultiBackend('yolov5s.pt', device=torch.device('cpu'))
 
     # profiling : memory, inference, total_time, gpu_utilization
-    models.append(['sqt10', squeezenet1_0(), 58, {'infer_time':3.96, 'total_time':10.0, 'gpu_util':47}])
-    models.append(['sqt11', squeezenet1_1(), 46, {'infer_time':3.52, 'total_time':9.62, 'gpu_util':40}])
-    models.append(['rnt18', resnet18(), 124, {'infer_time':4.39, 'total_time':24.2, 'gpu_util':54}])
-    models.append(['rnt34', resnet34(), 166, {'infer_time':7.54, 'total_time':43.22, 'gpu_util':54}])
-    models.append(['rnt50', resnet50(), 192, {'infer_time':13.44, 'total_time':59.84, 'gpu_util':56}])
-    models.append(['rnt101', resnet101(), 266, {'infer_time':22.75, 'total_time':132.62, 'gpu_util':50}])
-    models.append(['rnt152', resnet152(), 334, {'infer_time':32.86, 'total_time':179.28, 'gpu_util':55}])
-    models.append(['alexnet', alexnet(), 264, {'infer_time':4.24, 'total_time':136.29, 'gpu_util':39}])
-    models.append(['vgg11', vgg11(), 618, {'infer_time':13.39, 'total_time':304.11, 'gpu_util':43}])
-    models.append(['vgg13', vgg13(), 634, {'infer_time':18.59, 'total_time':306.89, 'gpu_util':44}])
-    models.append(['vgg16', vgg16(), 674, {'infer_time':23.03, 'total_time':319.4, 'gpu_util':46}])
-    models.append(['yolov5x', yolov5x, 422, {'infer_time':31.91, 'total_time':140.8, 'gpu_util':70}])
-    models.append(['yolov5s', yolov5s, 60, {'infer_time':7.46, 'total_time':27.29, 'gpu_util':40}])
-
-    weights = [x[2] for x in models]
-    prices = [x[3]['gpu_util'] for x in models]
+    models.append(['sqt10', squeezenet1_0(), {1:[58, {'infer_time':3.96, 'total_time':10.0, 'gpu_util':47}],
+                                              2:[120, {'infer_time':8.58, 'total_time':14.62, 'gpu_util':65}],
+                                              3:[186, {'infer_time':13.05, 'total_time':19.09, 'gpu_util':74}],
+                                              4:[238, {'infer_time':17.01, 'total_time':23.05, 'gpu_util':78}]}])
+    models.append(['sqt11', squeezenet1_1(), {1:[46, {'infer_time':3.52, 'total_time':9.62, 'gpu_util':40}],
+                                              2:[70, {'infer_time':4.45, 'total_time':10.55, 'gpu_util':51}],
+                                              3:[106, {'infer_time':6.84, 'total_time':12.94, 'gpu_util':60}],
+                                              4:[130, {'infer_time':9.03, 'total_time':15.13, 'gpu_util':67}]}])
+    models.append(['rnt18', resnet18(), {1:[124, {'infer_time':4.39, 'total_time':24.2, 'gpu_util':54}],
+                                         2:[126, {'infer_time':8.29, 'total_time':28.1, 'gpu_util':60}],
+                                         3:[154, {'infer_time':11.99, 'total_time':31.8, 'gpu_util':67}],
+                                         4:[196, {'infer_time':15.3, 'total_time':35.11, 'gpu_util':69}]}])
+    models.append(['rnt34', resnet34(), {1:[166, {'infer_time':7.54, 'total_time':43.22, 'gpu_util':54}],
+                                         2:[168, {'infer_time':14.48, 'total_time':50.16, 'gpu_util':61}],
+                                         3:[198, {'infer_time':21.12, 'total_time':56.8, 'gpu_util':67}],
+                                         4:[238, {'infer_time':27.37, 'total_time':63.05, 'gpu_util':71}]}])
+    models.append(['rnt50', resnet50(), {1:[192, {'infer_time':13.44, 'total_time':59.84, 'gpu_util':56}],
+                                         2:[238, {'infer_time':24.62, 'total_time':71.02, 'gpu_util':61}],
+                                         3:[274, {'infer_time':37.04, 'total_time':83.44, 'gpu_util':71}],
+                                         4:[192, {'infer_time':48.22, 'total_time':94.62, 'gpu_util':71}]}])
+    models.append(['rnt101', resnet101(), {1:[266, {'infer_time':22.75, 'total_time':132.62, 'gpu_util':50}],
+                                           2:[312, {'infer_time':40.81, 'total_time':150.68, 'gpu_util':61}],
+                                           3:[376, {'infer_time':63.33, 'total_time':173.2, 'gpu_util':68}],
+                                           4:[432, {'infer_time':83.93, 'total_time':193.8, 'gpu_util':72}]}])
+    models.append(['rnt152', resnet152(), {1:[334, {'infer_time':32.86, 'total_time':179.28, 'gpu_util':55}],
+                                           2:[354, {'infer_time':59.28, 'total_time':205.7, 'gpu_util':59}],
+                                           3:[400, {'infer_time':94.24, 'total_time':240.66, 'gpu_util':64}],
+                                           4:[494, {'infer_time':124.54, 'total_time':270.96, 'gpu_util':75}]}])
+    models.append(['alexnet', alexnet(), {1:[264, {'infer_time':4.24, 'total_time':136.29, 'gpu_util':39}],
+                                          2:[264, {'infer_time':4.71, 'total_time':136.76, 'gpu_util':39}],
+                                          3:[276, {'infer_time':6.11, 'total_time':138.16, 'gpu_util':42}],
+                                          4:[292, {'infer_time':6.85, 'total_time':138.9, 'gpu_util':42}]}])
+    models.append(['vgg11', vgg11(), {1:[618, {'infer_time':13.39, 'total_time':304.11, 'gpu_util':43}],
+                                      2:[742, {'infer_time':24.3, 'total_time':315.02, 'gpu_util':45}],
+                                      3:[848, {'infer_time':35.53, 'total_time':326.25, 'gpu_util':50}],
+                                      4:[944, {'infer_time':47.06, 'total_time':337.78, 'gpu_util':55}]}])
+    models.append(['vgg13', vgg13(), {1:[634, {'infer_time':18.59, 'total_time':306.89, 'gpu_util':44}],
+                                      2:[774, {'infer_time':35.35, 'total_time':323.65, 'gpu_util':50}],
+                                      3:[894, {'infer_time':53.64, 'total_time':341.94, 'gpu_util':58}],
+                                      4:[1006, {'infer_time':73.81, 'total_time':362.11, 'gpu_util':66}]}])
+    models.append(['vgg16', vgg16(), {1:[674, {'infer_time':23.03, 'total_time':319.4, 'gpu_util':46}],
+                                      2:[794, {'infer_time':44.92, 'total_time':341.29, 'gpu_util':55}],
+                                      3:[914, {'infer_time':71.11, 'total_time':367.48, 'gpu_util':66}],
+                                      4:[1026, {'infer_time':99.23, 'total_time':395.6, 'gpu_util':78}]}])
+    models.append(['yolov5x', yolov5x, {1:[422, {'infer_time':31.91, 'total_time':140.8, 'gpu_util':70}],
+                                        2:[458, {'infer_time':58.69, 'total_time':167.58, 'gpu_util':78}],
+                                        3:[506, {'infer_time':90.23, 'total_time':199.12, 'gpu_util':78}],
+                                        4:[566, {'infer_time':118.76, 'total_time':227.65, 'gpu_util':78}]}])
+    models.append(['yolov5s', yolov5s, {1:[60, {'infer_time':7.46, 'total_time':27.29, 'gpu_util':40}],
+                                        2:[96, {'infer_time':7.51, 'total_time':27.34, 'gpu_util':46}],
+                                        3:[110, {'infer_time':11.22, 'total_time':31.05, 'gpu_util':54}],
+                                        4:[132, {'infer_time':13.75, 'total_time':33.58, 'gpu_util':57}]}])
 
     opt = parse_opt()
     main(opt)
